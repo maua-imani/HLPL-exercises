c2706a33ea5711d53ed18c3f818d8ccf8c36da8a
Maua Imani <imanimaua@gmail.com>
2024-04-05 14:47:54 +0200

    A few updates

--- greatest.c
+++ greatest.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(){
-    int count = 0;
-    while(1){
     int len;
-    scanf("%d", &len); 
-    if(len == 0){
-        break;
-    }
-    int max;
-    scanf("%d", &max);
-    count = 0; 
-    for(int i =1; i <= len; i++){
-    int num;
-    scanf("%d", &num);
-    if(num > max){
-        max = num;
-        count = 1;
-    }
-    if(num == max){
-        count++;
-    }
-    }
+    int count;
     
-   printf("%d\n", count);
-    }
-    return EXIT_SUCCESS;
 
+    while(1){
+
+        count = 0;
+        scanf("%d", &len);
+        if(len == 0){
+            break;
+        }
+        int nums[len];
+        int max;
+        for(int i=0; i < len; i++){
+            scanf("%d", &nums[i]);
+        }
+        for(int j=0; j < len; j++){
+            max = nums[0];
+            if(nums[j]> max){
+                max = nums[j];
+            }
+        }
+
+        for(int k = 0; k < len; k++){
+            if(max==nums[k]){
+                count++;
+
+            }
+        }
+       printf("%d\n", count); 
+
+    }
+
+
+
+
+
+    return EXIT_SUCCESS;
 }
--- tester.c
+++ tester.c
@@ -1,21 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
 
-int main(){
-    int num;
-    int max;
+    int length;
+    
 
-    scanf("%d", &num);
-    max=num;
+    while(1)
+    { 
+    int count=0;
+    scanf("%d", &length);
+    if(length==0)
+    {
+        break;
+    }
 
-    while(scanf("%d", &num) != EOF){
-        if(num > max){
-            max=num;
+    int numbers[length];
+    int maximum;
+
+    for(int k=0; k<length; k++)
+    {
+        scanf("%d", &numbers[k]);
+    }
+
+    
+    for(int i=0; i<length;i++)
+    {
+         maximum=numbers[i];
+        if(numbers[i]> maximum)
+        {
+          maximum = numbers[i];
         }
     }
 
+    for(int j=0; j<length; j++)
+    {
+        if(maximum==numbers[j])
+        {
+            count++;
+        }
+    }
+    printf("%d\n", count);
 
-printf("The biggest number computed is %d\n", max);
 
-    return EXIT_SUCCESS;
+
+    }
+
+    
+
+return EXIT_SUCCESS;
 }
