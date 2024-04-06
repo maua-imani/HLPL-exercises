5cbe7869c9846f798a8a89af05259f1eb73e2a52
Maua Imani <imanimaua@gmail.com>
2024-04-06 15:07:57 +0200

    Made changes

--- local_mins.c
+++ local_mins.c
@@ -2,30 +2,13 @@
 #include <stdlib.h>
 
 
-
-int is_local(int *arr, int len)
-{
-if(len < 3){
-    return 0;
-}
-    if(arr[0]< arr[1]){
-    return 1;
-}
-    if(arr[len -1]< arr[len -2]){
-    return 1;
-}
-for(int i=1; i < len-1; i++){
-    if(arr[i]< arr[i-1] && arr[i]< arr[i+1]){
-        return 1;
-    }
-}
-return 0;
-}
-
 int count_local(int *arr, int len){
+    if(len < 3){
+        return 0;
+    }
     int count = 0;
-    for(int i=0; i < len; i++){
-        if(is_local(&arr[i], len)){
+    for(int i = 1; i < len -1; i++){
+        if(arr[i]< arr[i-1] && arr[i] < arr[i+1]){
             count++;
         }
     }
